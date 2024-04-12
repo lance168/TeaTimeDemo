a4724f0a50abc0599e4536e4fe7e60ac5173713e
LANCE-NB\Lance <lance168@gmail.com>
2024-04-12 10:49:45 +0800

    Add

---
+++ TeaTimeDemo/Controllers/CategoryController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TeaTimeDemo.Data;
+using TeaTimeDemo.Models;
+
+namespace TeaTimeDemo.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "類別名稱不能與顯示順序一樣!");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "類別新增成功!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categortFromDb = _db.Categories.Find(id);
+
+            if (categortFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categortFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categortFromDb = _db.Categories.Find(id);
+
+            if (categortFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categortFromDb);
+
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            //TODO: Confirm: 不知道為什麼物能直接傳物件進來，但是Edit就可以???
+            //直接傳obj.Name、obj.DisplayOrder的資料會是空的
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TeaTimeDemo/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeaTimeDemo.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("類別名稱")]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [DisplayName("顯示順序")]
+        [Range(1, 100,ErrorMessage ="輸入的資料範圍須在1~100")]
+        public int DisplayOrder { get; set; }
+
+    }
+}
