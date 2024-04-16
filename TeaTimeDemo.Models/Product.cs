7843d6bfbbc2f1927a75b85721c792d51087942b
LANCE-NB\Lance <lance168@gmail.com>
2024-04-16 14:42:08 +0800

    ViewData實作

--- TeaTimeDemo.Models/Product.cs
+++ TeaTimeDemo.Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -22,9 +23,10 @@ namespace TeaTimeDemo.Models
         public int CategoryId {  get; set; }
 
         [ForeignKey("CategoryId")]
-        public Category Category {  get; set; }
+        [ValidateNever]
+        public Category? Category {  get; set; }
 
-        public string ImageUrl {  get; set; }
+        public string? ImageUrl {  get; set; }
 
     }
 }
--- TeaTimeDemo/Areas/Admin/Controllers/ProductController.cs
+++ TeaTimeDemo/Areas/Admin/Controllers/ProductController.cs
@@ -31,7 +31,8 @@ namespace TeaTimeDemo.Areas.Admin.Controllers
                 Text = u.Name,
                 Value = u.Id.ToString()
             });
-            ViewBag.CategoryList = CategoryList;
+            //ViewBag.CategoryList = CategoryList;
+            ViewData["CategoryList"]= CategoryList;
             return View();
         }
 
