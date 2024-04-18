9c4a0b702895ca16a35f1ca7a45a48b20f9ce23f
LANCE-NB\Lance <lance168@gmail.com>
2024-04-18 13:53:34 +0800

    實作DataTable

--- TeaTimeDemo/Areas/Admin/Controllers/ProductController.cs
+++ TeaTimeDemo/Areas/Admin/Controllers/ProductController.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.IdentityModel.Tokens;
-using TeaTimeDemo.DataAcess.Data;
 using TeaTimeDemo.DataAcess.Repository.IRepository;
 using TeaTimeDemo.Models;
 using TeaTimeDemo.Models.ViewModels;
@@ -9,133 +9,21 @@ using TeaTimeDemo.Models.ViewModels;
 namespace TeaTimeDemo.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    //[Authorize(Roles = SD.Role_Admin + "," + SD.Role_Manager)]
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IWebHostEnvironment _webHostEnvironment;
-
         public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
             _webHostEnvironment = webHostEnvironment;
         }
-
         public IActionResult Index()
         {
-            //List<Product> objProductList = _ProductRepo.GetAll().ToList();
             List<Product> objCategoryList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
             return View(objCategoryList);
         }
-
-        //public IActionResult Create()
-        //{
-        //    //IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
-        //    //{
-        //    //    Text = u.Name,
-        //    //    Value = u.Id.ToString()
-        //    //});
-        //    //ViewBag.CategoryList = CategoryList;
-        //    //ViewData["CategoryList"]= CategoryList;
-        //    ProductVM productVM = new()
-        //    {
-        //        CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
-        //        {
-        //            Text = u.Name,
-        //            Value = u.Id.ToString()
-        //        }),
-        //        Product = new Product()
-        //    };
-
-        //    return View(productVM);
-        //}
-
-        //[HttpPost]
-        //public IActionResult Create(ProductVM obj)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        _unitOfWork.Product.Add(obj.Product);
-        //        _unitOfWork.Save();
-        //        TempData["Success"] = "產品新增成功!";
-        //        return RedirectToAction("Index");
-        //    }
-        //    else
-        //    {
-        //        ProductVM productVM = new()
-        //        {
-        //            CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
-        //            {
-        //                Text = u.Name,
-        //                Value = u.Id.ToString()
-        //            }),
-        //            Product = new Product()
-        //        };
-
-        //        return View(productVM);
-        //    }
-        //}
-
-        //public IActionResult Edit(int? id)
-        //{
-        //    if (id == null || id == 0)
-        //    {
-        //        return NotFound();
-        //    }
-        //    Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-
-        //    if (productFromDb == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(productFromDb);
-        //}
-
-        //[HttpPost]
-        //public IActionResult Edit(Product obj)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        _unitOfWork.Product.Update(obj);
-        //        _unitOfWork.Save();
-        //        TempData["Success"] = "產品編輯成功!";
-        //        return RedirectToAction("Index");
-        //    }
-        //    return View();
-        //}
-
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-
-            return View(productFromDb);
-
-        }
-
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeletePost(int? id)
-        {
-            //TODO: Confirm: 不知道為什麼物能直接傳物件進來，但是Edit就可以???
-            //直接傳obj.Name、obj.DisplayOrder的資料會是空的
-            Product? obj = _unitOfWork.Product.Get(u => u.Id == id);
-            if (obj == null)
-            {
-                return NotFound();
-            }
-            _unitOfWork.Product.Remove(obj);
-            _unitOfWork.Save();
-            return RedirectToAction("Index");
-        }
-
         public IActionResult Upsert(int? id)
         {
             ProductVM productVM = new()
@@ -147,47 +35,42 @@ namespace TeaTimeDemo.Areas.Admin.Controllers
                 }),
                 Product = new Product()
             };
-
-            //新增
             if (id == null || id == 0)
             {
                 return View(productVM);
             }
-            else //修改
+            else
             {
-                productVM.Product = _unitOfWork.Product.Get(u => u.Id.Equals(id));
+                productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
                 return View(productVM);
             }
         }
-
         [HttpPost]
+        // 本次修改部分
         public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
             if (ModelState.IsValid)
             {
-                string webRootPath = _webHostEnvironment.WebRootPath;
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
                 if (file != null)
                 {
                     string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
-                    string productPath = Path.Combine(webRootPath, @"images\product");
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
 
-                    //如果原本有檔案，就把檔案刪除，更新為新檔案
-                    if (!productVM.Product.ImageUrl.IsNullOrEmpty())
+                    if (!string.IsNullOrEmpty(productVM.Product.ImageUrl))
                     {
-                        var oldImagePath = Path.Combine(webRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+                        var oldImagePath = Path.Combine(wwwRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
                         if (System.IO.File.Exists(oldImagePath))
                         {
                             System.IO.File.Delete(oldImagePath);
                         }
                     }
-
                     using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
                     {
                         file.CopyTo(fileStream);
                     }
                     productVM.Product.ImageUrl = @"\images\product\" + fileName;
                 }
-
                 if (productVM.Product.Id == 0)
                 {
                     _unitOfWork.Product.Add(productVM.Product);
@@ -196,9 +79,8 @@ namespace TeaTimeDemo.Areas.Admin.Controllers
                 {
                     _unitOfWork.Product.Update(productVM.Product);
                 }
-
                 _unitOfWork.Save();
-                TempData["Success"] = "產品新增成功!";
+                TempData["success"] = "產品新增成功！";
                 return RedirectToAction("Index");
             }
             else
@@ -211,5 +93,38 @@ namespace TeaTimeDemo.Areas.Admin.Controllers
                 return View(productVM);
             }
         }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+            return Json(new { data = objProductList });
+        }
+
+        [HttpGet] //不能用HttpDelete 會出現 HTTP ERROR 405 
+        public IActionResult Delete(int? id)
+        {
+            var productToBeDeleted = _unitOfWork.Product.Get(u => u.Id == id);
+            if (productToBeDeleted == null)
+            {
+                return Json(new { success = false, message = "刪除失敗" });
+            }
+
+            if (!productToBeDeleted.ImageUrl.IsNullOrEmpty())
+            {
+                var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, productToBeDeleted.ImageUrl.TrimStart('\\'));
+                if (System.IO.File.Exists(oldImagePath))
+                {
+                    System.IO.File.Delete(oldImagePath);
+                }
+            }
+
+            _unitOfWork.Product.Remove(productToBeDeleted);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "刪除成功" });
+        }
+        #endregion
     }
 }
